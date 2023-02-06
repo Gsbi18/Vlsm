31d024d9bc1bb620bd8490775b3f3928c2d2bc3e
Gsbi18 <74606522+Gsbi18@users.noreply.github.com>
2023-02-06 16:18:40 +0100

    Add files via upload

--- Új mappa/script.js
+++ Új mappa/script.js
@@ -1,10 +1,14 @@
-var i =0;
-function Apply(){
-    subnets_num=document.getElementById("subnet").value;
-    
-    let text="";
-    for (let i = 0; i < subnets_num; i++) {
-        text+="<input type='text'placeholder='NAME' class='bevit vlan'>"+"<input type='number' placeholder='NUMBERS' class='vlan bevit'>" + "<br>"
-      }
-      document.getElementById("subnet_reveal").innerHTML=text;
+function Apply() {
+  subnets_num = document.getElementById("subnet").value;
+  netmask = document.getElementById("netmask").value;
+  ip = document.getElementById("ip").value;
+  console.log("IP:"+ip)
+  console.log("NETMASK:"+netmask)
+
+  let text = "";
+  for (let i = 0; i < subnets_num; i++) {
+    text += '<input type="text" placeholder="NAME" id="name' + [i] + '" class="bevit vlan">' + '<input type="number" placeholder="NUMBERS" id="number' + [i] + ' "class="vlan bevit">' + '<br>'
+  }
+  document.getElementById("subnet_reveal").innerHTML = text;
+  document.getElementById("subnet_reveal").innerHTML += '<input type="button" id="apply"  value="Generate">';
 }
